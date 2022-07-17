488d8e4e9bf3c7931aad8cff87b5e84d66f0440f
Sheehan Maitra <maitrasheehan@gmail.com>
2022-07-16 20:18:58 -0400

    ppp

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PFP from "../assests/pfp.png"
+import PFP from "../assests/pfp.png";
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#1A1D3D] text-[#BFBDE8]">
@@ -13,19 +13,32 @@ const About = () => {
           <div></div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 px-3">
-          <div className="sm:text-left text-4xl font-bold ">
-            <p>Hi. I'm Sheehan, nice to meet you. Feel free to take a look around.</p>
+          <div>
+            <p className="sm:text-left text-4xl font-bold">
+              Hi. I'm Sheehan, nice to meet you. Feel free to take a look
+              around.
+            </p>
+            <p className="item-body py-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              expedita corporis eaque nobis enim harum ut repudiandae obcaecati.
+              Et dicta nostrum minus mollitia deleniti maiores rerum nam est
+              officiis magni cupiditate, at quibusdam qui id. Porro maxime magni
+              fuga omnis a exercitationem consectetur quas architecto, obcaecati
+              perspiciatis molestias reiciendis ipsam numquam, hic id. Sit quis
+              beatae provident mollitia reiciendis quia pariatur soluta, ad
+              debitis dolores veritatis? Error natus modi vero sapiente quaerat,
+              aperiam eos hic consequuntur quas corrupti assumenda sint qui
+              cupiditate accusamus suscipit possimus facere earum, omnis,
+              expedita ullam maxime delectus nostrum dignissimos recusandae.
+              Veniam tenetur quibusdam optio itaque!
+            </p>
           </div>
           <div>
+            <span class='tint'>
+            <img className="w-[250px]" src={PFP} alt="profilepicture" />
+            </span>
           </div>
-          <div><p className="item-body py-6">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis,
-              error minus sint a consequuntur beatae veniam, explicabo nihil
-              nisi placeat accusamus dolorem dolore, ex fugiat.
-            </p></div>
-            <div>
-            <img className="w-90 h-90"src={PFP} alt="profilepicture" />  
-            </div>
+          <div></div>
         </div>
       </div>
     </div>
